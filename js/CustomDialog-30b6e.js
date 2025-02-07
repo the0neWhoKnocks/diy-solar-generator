d72fc1754f17502b35753503c264487c0aa3592e
the0neWhoKnocks <the0neWhoKnocks@users.noreply.github.com>
2025-02-07 20:26:56 +0000

    Deploying to gh-pages from @ the0neWhoKnocks/diy-solar-generator@4a255aafc70ab183f53ad3691e49c21dfdb5075a 🚀

--- js/CustomDialog-30b6e.js
+++ js/CustomDialog-30b6e.js
@@ -1,4 +1,4 @@
-(()=>{const c="--dialog-animDuration",l="--dialog--color--body",a="--dialog--color--border",i="--dialog--color--title--bg",n="--dialog--color--title--text",d="dialogClosed",t="dialog",m="#eee",h="#000",p="#333",u="#eee",b="[BODY]";class _ extends HTMLElement{get modal(){return this.hasAttribute("modal")}set modal(e){e===""||e==="true"||e===!0?this.setAttribute("modal",""):this.removeAttribute("modal"),this.render()}get open(){return this.hasAttribute("open")}set open(e){e===""||e==="true"||e===!0?(this.render(),this.setAttribute("open",""),this.els.dialog.setAttribute("open",""),this.els.wrapper.classList.add("in"),setTimeout(()=>{this.els.wrapper.addEventListener("click",this.handleCloseClick),window.addEventListener("keydown",this.handleKeyDown)},300)):(this.els.wrapper.removeEventListener("click",this.handleCloseClick),window.removeEventListener("keydown",this.handleKeyDown),this.els.wrapper.classList.add("out"),setTimeout(()=>{this.els.wrapper.classList.remove("in","out"),this.removeAttribute("open"),this.els.dialog.removeAttribute("open"),this.dispatchEvent(new CustomEvent(d,{bubbles:!0,detail:{modal:this.modal}}))},300))}static get observedAttributes(){return["modal","open"]}static get events(){return{closed:d}}attributeChangedCallback(e,o,s){if(!(o===""&&s==null)&&o!==s){let $=s;switch(e){default:this[e]=$}}}constructor(){super(),this.attachShadow({mode:"open"});const{shadowRoot:e}=this;e.innerHTML=`
+(()=>{const _="--dialog-animDuration",l="--dialog--color--body",a="--dialog--color--border",i="--dialog--color--title--bg",n="--dialog--color--title--text",d="dialogClosed",e="dialog",m="#eee",h="#000",p="#333",u="#eee",b="[BODY]";class c extends HTMLElement{get modal(){return this.hasAttribute("modal")}set modal(t){t===""||t==="true"||t===!0?this.setAttribute("modal",""):this.removeAttribute("modal"),this.render()}get open(){return this.hasAttribute("open")}set open(t){t===""||t==="true"||t===!0?(this.prevFocused=document.activeElement,this.render(),this.setAttribute("open",""),this.els.dialog.setAttribute("open",""),this.els.wrapper.classList.add("in"),setTimeout(()=>{this.els.wrapper.addEventListener("click",this.handleCloseClick),window.addEventListener("keydown",this.handleKeyDown),this.els.dialog.focus()},300)):(this.els.wrapper.removeEventListener("click",this.handleCloseClick),window.removeEventListener("keydown",this.handleKeyDown),this.els.wrapper.classList.add("out"),setTimeout(()=>{this.els.wrapper.classList.remove("in","out"),this.removeAttribute("open"),this.els.dialog.removeAttribute("open"),this.prevFocused&&this.prevFocused.focus(),this.dispatchEvent(new CustomEvent(d,{bubbles:!0,detail:{modal:this.modal}}))},300))}static get observedAttributes(){return["modal","open"]}static get events(){return{closed:d}}attributeChangedCallback(t,o,s){if(!(o===""&&s==null)&&o!==s){let $=s;switch(t){default:this[t]=$}}}constructor(){super(),this.attachShadow({mode:"open"});const{shadowRoot:t}=this;t.innerHTML=`
         <style>
           @keyframes dialogIn {
             0% {
@@ -34,7 +34,7 @@
           }
           
           :host {
-            ${c}: 300ms;
+            ${_}: 300ms;
             ${l}: ${m};
             ${a}: ${h};
             ${i}: ${p};
@@ -59,7 +59,7 @@
             cursor: pointer;
           }
           
-          .${t}-wrapper {
+          .${e}-wrapper {
             width: 100vw;
             height: 100vh;
             display: flex;
@@ -67,7 +67,7 @@
             align-items: center;
           }
           
-          .${t}__body button {
+          .${e}__body button {
             color: #fff;
             width: 100%;
             padding: 0.75em 1em;
@@ -76,10 +76,10 @@
             background: #000;
             position: relative;
           }
-          .${t}__body button:focus {
+          .${e}__body button:focus {
             outline: none;
           }
-          .${t}__body button:focus::after {
+          .${e}__body button:focus::after {
             content: '';
             position: absolute;
             border: solid 2px currentColor;
@@ -90,7 +90,7 @@
             right: 2px;
           }
           
-          .${t} {
+          .${e} {
             max-width: calc(100vw - 2em);
             max-height: calc(100vh - 2em);
             overflow: hidden;
@@ -101,7 +101,7 @@
             box-shadow: 0 0.75em 2em 0.25em rgba(0, 0, 0, 0.75);
           }
           
-          .${t}__nav {
+          .${e}__nav {
             min-height: 2em;
             font-size: 1.25em;
             border-bottom: solid 1px;
@@ -109,7 +109,7 @@
             display: flex;
           }
           
-          .${t}__title {
+          .${e}__title {
             width: 100%;
             color: var(${n});
             padding: 0.5em;
@@ -117,22 +117,27 @@
             background: var(${i});
           }
           
-          .${t}__body {
+          .${e}__body {
             background: var(${l});
           }
           
-          .${t}__close-btn {
+          .${e}__close-btn {
             color: var(${n});
             font-size: 1.25em;
             padding: 0 0.5em;
             border: none;
+            border-radius: 0.3em; /* for focus border */
             background: var(${i});
           }
-          :host([modal]) .${t}__close-btn {
+          .${e}__close-btn:focus-visible {
+            outline: solid 2px color-mix(in srgb, currentColor 40%, transparent);
+            outline-offset: -4px;
+          }
+          :host([modal]) .${e}__close-btn {
             display: none;
           }
           
-          .${t}-mask {
+          .${e}-mask {
             width: 100%;
             height: 100%;
             background: rgba(255, 255, 255, 0.5);
@@ -142,38 +147,38 @@
             backdrop-filter: blur(10px);
           }
           
-          .${t}-wrapper.in .${t}-mask {
+          .${e}-wrapper.in .${e}-mask {
             animation: maskIn 300ms;
           }
-          .${t}-wrapper.out .${t}-mask {
+          .${e}-wrapper.out .${e}-mask {
             animation: maskOut 300ms forwards;
           }
-          .${t}-wrapper.in .${t} {
+          .${e}-wrapper.in .${e} {
             animation: dialogIn 300ms;
           }
-          .${t}-wrapper.out .${t} {
+          .${e}-wrapper.out .${e} {
             animation: dialogOut 300ms forwards;
           }
         </style>
         
-        <div class="${t}-wrapper">
-          <div class="${t}-mask"></div>
-          <dialog class="${t}" tabindex="0"></dialog>
+        <div class="${e}-wrapper">
+          <div class="${e}-mask"></div>
+          <dialog class="${e}" tabindex="0"></dialog>
         </div>
-      `,this.els={dialog:e.querySelector(`.${t}`),wrapper:e.querySelector(`.${t}-wrapper`)},this.handleCloseClick=this.handleCloseClick.bind(this),this.handleKeyDown=this.handleKeyDown.bind(this)}handleCloseClick({keyClose:e,target:o}){!this.modal&&(e||o&&(o.classList.contains(`${t}-mask`)||o.classList.contains(`${t}__close-btn`)))&&(this.open=!1)}handleKeyDown({key:e}){switch(e){case"Escape":this.handleCloseClick({keyClose:!0});break}}render(){const e=this.querySelector('[slot="dialogTitle"]');let o="";if(!this.modal||this.modal&&e){let s="";this.modal||(s=`
-            <button type="button" class="${t}__close-btn" title="Close Dialog">
+      `,this.els={dialog:t.querySelector(`.${e}`),wrapper:t.querySelector(`.${e}-wrapper`)},this.handleCloseClick=this.handleCloseClick.bind(this),this.handleKeyDown=this.handleKeyDown.bind(this)}handleCloseClick({keyClose:t,target:o}){!this.modal&&(t||o&&(o.classList.contains(`${e}-mask`)||o.classList.contains(`${e}__close-btn`)))&&(this.open=!1)}handleKeyDown({key:t}){switch(t){case"Escape":this.handleCloseClick({keyClose:!0});break}}render(){const t=this.querySelector('[slot="dialogTitle"]');let o="";if(!this.modal||this.modal&&t){let s="";this.modal||(s=`
+            <button type="button" class="${e}__close-btn" title="Close Dialog">
               &#10005;
             </button>
           `),o=`
-          <nav class="${t}__nav">
-            <div class="${t}__title">
+          <nav class="${e}__nav">
+            <div class="${e}__title">
               <slot name="dialogTitle"></slot>
             </div>
             ${s}
           </nav>
         `}this.els.dialog.innerHTML=`
         ${o}
-        <div class="${t}__body">
+        <div class="${e}__body">
           <slot name="dialogBody">${b}</slot>
         </div>
-      `}}const r="custom-dialog";window.customElements.get(r)?console.warn(`${r} already defined`):(window.customElements.define(r,_),window.CustomDialog=_)})();
+      `}}const r="custom-dialog";window.customElements.get(r)?console.warn(`${r} already defined`):(window.customElements.define(r,c),window.CustomDialog=c)})();
